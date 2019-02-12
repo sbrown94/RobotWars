f7eec7de6a55d5e3467d091f80f4406c30edc111
Scott <scott.brown999@gmail.com>
2019-02-12 19:47:44 +0000

    set up project and test project -- add some questions to the readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# robotwars
+
+### Questions ###
+
+What should happen when the robot is given a command that would take it outside of the arena? Should it ignore the command or fall off the stage?
+
+---
+
+Is it safe to assume there were only ever be the robot and nothing else on the stage?
+
+---
+
---
+++ robotwars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robotwars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = Program.PrintHello();
+        }
+
+        int PrintHello()
+        {
+            Console.WriteLine("Hello World");
+            return 1;
+        }
+    }
+}
