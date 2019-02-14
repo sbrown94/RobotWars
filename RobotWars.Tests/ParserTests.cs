16650539658dbf8a0a2fd780b87ce8f55fa530b7
Scott <scott.brown999@gmail.com>
2019-02-14 19:59:56 +0000

    updated ParserTests

--- RobotWars.Tests/ParserTests.cs
+++ RobotWars.Tests/ParserTests.cs
@@ -7,15 +7,32 @@ namespace RobotWars.Tests
 {
     class ParserTests
     {
-        [TestCase("alksjhd", "Not enough data to run simulation.")]
-        [TestCase("5 5\r\n9 3 E\r\nMRRRLLR", "none")]
-        [TestCase("5 5\r\n9 3 E\r\nMRRDEASDLR", "Invalid characters in instructions for Robot 1")]
-        public void TestInputIsValid(string input, string expected)
+        [TestCase("5 5\r\n4 3 E\r\nMRRRLLR")]
+        public void TestInputIsValid(string input)
         {
-            var commandParser = new CommandParser();
-            var data = commandParser.GetCommands(input);
-            var isValid = commandParser.Validate(data);
-            Assert.AreEqual(isValid, expected);
+            var fileParser = new FileParser();
+            var data = fileParser.GetCommandsAsStringList(input);
+            var isValid = fileParser.Validate(data);
+            Assert.IsTrue(isValid);
+        }
+
+        [TestCase("5 5\r\n4 3 E\r\nMRRDEASDLR", "Invalid characters in instructions for Robot 1")]
+        [TestCase("5 5\r\n2 2 W\r\nMMRRMMLL\r\n2 4 E", "Each robot must have a starting location and instructions")]
+        public void TestInputIsInvalid(string input, string expected)
+        {
+            var fileParser = new FileParser();
+            var data = fileParser.GetCommandsAsStringList(input);
+            var ex = Assert.Throws<Exception>(() => fileParser.Validate(data));
+            Assert.That(ex.Message, Is.EqualTo(expected));
+        }
+
+        [TestCase("alksjhd", "Input string was not in a correct format.")]
+        public void TestInputFormatIsValid(string input, string expected)
+        {
+            var fileParser = new FileParser();
+            var data = fileParser.GetCommandsAsStringList(input);
+            var ex = Assert.Throws<FormatException>(() => fileParser.Validate(data));
+            Assert.That(ex.Message, Is.EqualTo(expected));
         }
     }
 }
