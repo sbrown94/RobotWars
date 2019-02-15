0a9925993f96d70052934a9084eef3bfcd365936
Scott Brown <scott.brown@steinias.com>
2019-02-15 09:43:01 +0000

    add integration test -- restructure test project -- add sample commands.txt to root

---
+++ RobotWars.Tests/Integration/IntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotWars.Tests
+{
+    class IntegrationTests
+    {
+        // To check full integration including File IO, make sure commands.txt is
+        // in RobotWars.Tests/bin/Debug/netcoreapp2.1
+        [Test]
+        public void TestRunFullSimulation()
+        {
+            // Arrange
+            MainController mControl = new MainController();
+            List<string> output = new List<string>();
+            List<string> expectedOutput = new List<string>();
+            expectedOutput.Add("1 3 N");
+            expectedOutput.Add("5 1 E");
+
+            // Act
+            output = mControl.InitSimulation();
+
+            // Assert
+            Assert.AreEqual(output.Count, expectedOutput.Count);
+            for (var i = 0; i < output.Count; i++)
+            {
+                Assert.AreEqual(output[i], expectedOutput[i]);
+            }
+        }
+    }
+}
